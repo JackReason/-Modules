f1dd3d73af8a464dff6f663d3d0c6aa621e1a144
Ruslan Kolomenskiy <charly100@yandex.ru>
2024-04-05 15:59:24 +0300

    Module 15/4

--- Module 13.1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Module 13.1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,18 +2,29 @@
 #include <iostream>
 #include <string>
 using namespace std;
+int OddFind(const int length, bool odd);
+const int N = 13;
 
 int main()
-{
-	string word1 = "Let's play us";
-	string word2 = ("some game");
-	string word3(3, '!');
-	string phrase = word1 + " " + word2 + word3;
-	cout << phrase << "\n";
-	cout << "The phrase has " << phrase.size() << " symbols in it\n";
-	cout << "The first symbol is : " << phrase[0] << "\n";
-	cout << "The last symbol is : " << phrase[phrase.length()-1];
-	return 0;
+{   
+	cout << "first task\n";
+	for (int i = 0; i <= N; i++)
+	{
+		if (i % 2 == 0 && i!=0)
+			cout << i << "\n";
+	}
+	cout << "Second task\n";
+     OddFind(N, true);
+	 
 
+	
+}
+int OddFind( const int lenght, bool odd)
+{
+	for (int i = 2-odd; i <= lenght; i += 2 )
+	{
+		cout << i << "\n";
+	}
+	return 0;
 }
 
