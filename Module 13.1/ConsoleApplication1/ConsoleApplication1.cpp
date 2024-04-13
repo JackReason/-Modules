4cd4882a14e1b1422789ce447567ab62f6774bae
Ruslan Kolomenskiy <charly100@yandex.ru>
2024-04-13 14:06:49 +0300

    Module 19

--- Module 13.1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Module 13.1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,95 +1,60 @@
 
 #include <iostream>
 #include <string>
-#include <algorithm>
 using namespace std;
-
-class player
+class Animal
 {
-private:
-	string name = "noname";
-	int points = 0;
 public:
-	void getName()
+	virtual void Voice()
 	{
-		cin >> name;
+		cout << "default sound";
 	}
-	string showName()
+	
+};
+
+class Piggy : public Animal
+{
+public:
+	void Voice() override
 	{
-		return name;
+		cout << "Squek";
 	}
-	void getPoints()
+
+};
+
+class Cow : public Animal
+{
+public:
+	void Voice() override
 	{
-		cin >> points;
-	}
-	int showPoints()
-	{
-		return points;
+		cout << "Muuu";
 	}
 };
 
-void masShuffle(player* pointer, int num); 
-void showBoard(player* pointer, int num);  
-
+class Horsy : public Animal
+{
+public:
+	void Voice() override
+	{
+		cout << "EeeeeHaaa";
+	}
+};
 
 int main()
 {
-	int numPlrs;
-	cout << "How many players?\n";
-	cin >> numPlrs;
-	player* p = new player[numPlrs];
-	cout << "\nSo we got : " << numPlrs << " players\n";
-	cout << "Let's name our players and give'em some scores\n";
-
-	for (int i = 0; i < numPlrs; i++)
+	Cow a;
+	Horsy b;
+	Piggy c;
+	Animal* arr[3] = { &a,&b,&c };
+	for (int i = 0; i < 3; i++)
 	{
-		cout << "\nplayer number " << (i + 1) << " name is ";
-		(p + i)->getName();
-		cout << (p + i)->showName() << "'s scores are : ";
-		(p + i)->getPoints();
-
-
+		arr[i]->Voice();
+		cout << ' ';
 	}
 	
-	cout << "\nso now we have this board\n";
-	showBoard(p, numPlrs);
-	cout << "\nLet us shuffle numbers a bit\n";
-	masShuffle(p, numPlrs);
-	showBoard(p, numPlrs);
-	delete[] p;
-	cout << "\nThe end";
-
 
 	
 
 }
 
 
-void masShuffle(player* pointer, int num)
-{
-	double factor = 1.25;
-	int step = num-1;
-	while (step >= 1)
-	{
-		for (int i = 0; i + step < num; i++)
-		{
-			if ((pointer + i)->showPoints() < (pointer + i + step)->showPoints())
-			{
-				std::swap(*(pointer + i), *(pointer + i + step));
-			}
-		}
-		step /= factor;
-	}
-}
-
-void showBoard(player* pointer, int num)
-{
-	for (int i = 0; i < num; i++)
-	{
-		cout << (pointer + i)->showName();
-		cout << " scores " << (pointer + i)->showPoints();
-		cout << "\n";
-
-	}
-
-}
